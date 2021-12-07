8a5d00d2849fbe00798d1641d774817c87f7d015
Luke Serraglio <45311902+Transparent-Radish@users.noreply.github.com>
2021-12-07 10:05:40 -0500

    added collisions

--- imagemain.cpp
+++ imagemain.cpp
@@ -2,14 +2,17 @@
 #include "FEHSD.h"
 #include "FEHUtility.h"
 #include <math.h>
+#include <stdio.h>
 // #include <iostream>
 // using namespace std;
-
+bool collisionBuffer[240][320];
 class Image
 {
     public:
         Image(const char *);
         void display(int, int);
+        bool addCollision(int,int);
+        void clearCollisions();
     private:
         unsigned int *data;
         const char *fn;
@@ -20,23 +23,24 @@ class Image
 Image::Image(const char *fname)
 {
     fn = fname;
-    FEHFile *fptr = SD.FOpen(fname, "r");
-    SD.FScanf(fptr, "%i", &w);
-    SD.FScanf(fptr, "%i", &h);
+    FILE *fptr = fopen(fname, "r");
+    fscanf(fptr, "%i", &w);
+    fscanf(fptr, "%i", &h);
     data = new unsigned int[w * h];
 
     for (int i = 0; i < w * h; i ++)
     {
-        SD.FScanf(fptr, "%i", &data[i]);
-    }
-    SD.FClose(fptr);
+        fscanf(fptr, "%ui", &data[i]);
 
-    // These next 4 lines are not necessary; they simply set the four corners 
-    // of the image to blue pixels, just to see the border of each image.
-    data[0] = 0xFF0000FF;
-    data[w - 1] = 0xFF0000FF;
-    data[(h - 1) * w ] = 0xFF0000FF;
-    data[w * h - 1] = 0xFF0000FF;
+    }
+    fclose(fptr);
+
+    // // These next 4 lines are not necessary; they simply set the four corners 
+    // // of the image to blue pixels, just to see the border of each image.
+    // data[0] = 0xFF0000FF;
+    // data[w - 1] = 0xFF0000FF;
+    // data[(h - 1) * w ] = 0xFF0000FF;
+    // data[w * h - 1] = 0xFF0000FF;
 }
 
 void Image::display(int x, int y)
@@ -48,20 +52,57 @@ void Image::display(int x, int y)
             unsigned int col = data[j * w + i];
             if (col & 0xFF000000 && y + j < 240 && y + j >= 0)
             {
-                LCD.SetFontColor(0xFFFFFFFF - col);
+                LCD.SetFontColor(col&0xffffff);
                 LCD.DrawPixel(x + i, y + j);
             }
         }
     }
 }
 
+//check if the image collides with other selected images
+bool Image::addCollision(int x,int y){
+    for (int i = 0; i < w; i++)
+    {
+        for (int j = 0; j < h; j++)
+        {   
+            //get the 32 bit color of the pixel
+            unsigned int col = data[j * w + i];
+            //if the pixel is on the screen and not transparent go on
+            if (col & 0xFF000000 && y + j < 240 && y + j >= 0)
+            {
+                //if the collision buffer wasn't already filled in on this pixel, fill it in
+                //if not, another pixel was here, and the shape collided with another shape, so return true
+                if(!collisionBuffer[y+j][x+i])
+                    collisionBuffer[y+j][x+i]=true;
+                else{
+                    //printf("%i,%i\n",y+j,x+i);
+                    return true;
+                }
+            }
+        }
+    }
+    //if no collisions were detected, return false
+    return false;
+}
+
+//clear the collision buffer
+void clearCollisions(){
+for (int i = 0; i < 320; i++)
+    {
+        for (int j = 0; j < 240; j++)
+        {
+            collisionBuffer[j][i]=false;
+        }
+    }
+}
+
 int main() 
 {
-    LCD.SetBackgroundColor(LCD.Blue);     // Clear background
+    //LCD.SetBackgroundColor(LCD.Blue);     // Clear background
     LCD.Clear();
 
     // background and foreground positions and velocities
-    float bgX = 50, bgVel = 1, fgX = 50, fgVel = 2;
+    float bgX = 50, bgVel = 1, fgX = 240, fgVel = 2;
 
     // variables for the bird's mechanics.
     float y = 0, yVel = 0, g = 1.3, bounceVel = -5;
@@ -79,12 +120,16 @@ int main()
     Image tPipe("s/pgd.txt");
     Image bPipe("s/pg.txt");
 
-
+    bool alive = true;
     int fF = 0;
-    while (1) 
+    while (alive) 
     {
         LCD.Update();
         LCD.Clear();
+
+        clearCollisions();
+
+
         y += yVel;
         yVel += g;
         bgX -= bgVel;
@@ -99,18 +144,27 @@ int main()
         tPipe.display(fgX, -240);
         bPipe.display(fgX, 150);
 
-        base.display(fgX, 195);
+        tPipe.addCollision(fgX, -240);
+        bPipe.addCollision(fgX, 150);
 
-        yb[(int) (fF / 3.0) % 4].display(50, (int) (sin(smooth) * 30) + 100);
+        base.display(fgX, 195);
+        base.addCollision(fgX, 195);
+
+
+        //yb[(int) (fF / 3.0) % 4].display(50, (int) (sin(smooth) * 30) + 100);
         yb[(int) (fF / 3.0) % 4].display(50, y);
         
  
-        if (LCD.Touch(&xt, &yt) || y > 240 - 24 - 5)
+        if (LCD.Touch(&xt, &yt)/* */)
         {
             yVel = bounceVel;
         }
+        if(yb[(int) (fF / 3.0) % 4].addCollision(50, y)|| y > 240 - 24 - 5){
+            alive = false;
+        }
+        
     }
-    return 0;
+    //return 0;
 }
 
 
